40b8671029256b722b6a4c8d5d0418ccd4e30f37
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-07-08 18:04:21 -0700

    minor updates and adjustments to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,8 +61,7 @@ var createTaskEl = function (taskDataObj) {
 
     tasks.push(taskDataObj);
 
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
+    saveTasks();
 };
 
 var createTaskActions = function (taskId) {
@@ -125,6 +124,7 @@ var completeEditTask = function (taskName, taskType, taskId) {
     formEl.removeAttribute("data-task-id");
     // update formEl button to go back to saying "Add Task" instead of "Edit Task"
     formEl.querySelector("#save-task").textContent = "Add Task";
+    saveTasks();
 };
 
 var taskButtonHandler = function (event) {
@@ -143,7 +143,6 @@ var taskButtonHandler = function (event) {
 };
 
 var taskStatusChangeHandler = function (event) {
-    console.log(event.target.value);
 
     // find task list item based on event.target's data-task-id attribute
     var taskId = event.target.getAttribute("data-task-id");
@@ -160,6 +159,7 @@ var taskStatusChangeHandler = function (event) {
     } else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+    saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -204,6 +204,7 @@ var deleteTask = function (taskId) {
 
     // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+    saveTasks();
 };
 
 var saveTasks = function () {
